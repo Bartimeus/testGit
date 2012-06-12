a7f295095cc01149e5128a1ecba998c50afcf53a
home <prokhorov_pd@mail.ru>
2012-06-12 18:56:28 +0400

    added label

--- testGit/testgit.cpp
+++ testGit/testgit.cpp
@@ -4,9 +4,16 @@ testGit::testGit(QWidget *parent, Qt::WFlags flags)
 	: QMainWindow(parent, flags)
 {
 	ui.setupUi(this);
+
+	QLabel *label = new QLabel(this);
 }
 
 testGit::~testGit()
 {
 
+}
+
+void testGit::showText()
+{
+	label->setText("ololo");
 }
--- testGit/testgit.h
+++ testGit/testgit.h
@@ -14,6 +14,8 @@ public:
 
 private:
 	Ui::testGitClass ui;
+
+	void showText();
 };
 
 #endif // TESTGIT_H
